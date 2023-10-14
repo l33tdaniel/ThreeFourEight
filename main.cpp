bc10a3f8f72e57a72cd33a97048f1215350d013c
Sabeen Ahmad <sabeen231@gmail.com>
2023-10-14 18:00:06 -0500

    fixed paranthesis function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
+using namespace std;
+
+bool isBalancedParanthesis(const string &expression){
+    stack<char> stack;
+    for(int i =0; i<expression.length();i++){
+        if (expression[i] == '(')
+        {
+            stack.push(expression[i]);
+        } else if(expression[i]== ')')
+        {
+            if (stack.empty()){
+                return false;
+            }
+            else{
+                stack.pop();
+            }
+        }
+    }
+    return stack.empty();
+}
 
 int main(){
     string expression;
     cout << "Enter an expression: " << endl;
     cin >> expression;
-    cout<<isBalancedParanthesis(expression)<<endl;
     return 0;
 }
 
