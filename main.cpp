b7cdc8ce8ea63d7755d500f8cce5069175466ec7
Brett Balquist <brett@balquist.com>
2023-10-14 17:13:55 -0500

    Added Modulo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-
+#include <stdexcept>
 int main(){
     std::cout << "Hello, World!" << std::endl;
     return 0;
+}
+
+int modulo(int operand1, int operand2){
+	if (operand2 != 0){
+		return operand1 % operand2;
+	}else{
+		throw std::invalid_argument( "Cannot use modulo with 0" );
+	}
 }
 
