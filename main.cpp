19bef0ad944cdf7de60fe3c73f8f5117037fbd0a
Sabeen Ahmad <sabeen231@gmail.com>
2023-10-14 17:45:26 -0500

    created a funciton to check paranthesis

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+bool isBalancedParanthesis(const string &expression){
+    stack <char> stack;
+    int count = 0;
+    for(int i = 0; i <expression.length();i++){
+        if (expression[i] == '(')
+        {
+            stack.push(expression[i]);
+            }
+        else if (expression[i] == ')'){
+            if (stack.empty()){
+                return false;
+            }
+            else{
+                stack.pop();
+            }
+        }
+        }
+    return stack.empty();
+    }
+
+
 
 int main(){
-    std::cout << "Hello, World!" << std::endl;
+    string expression;
+    cout << "Enter an expression: " << endl;
+    cin >> expression;
+    cout<<isBalancedParanthesis(expression)<<endl;
     return 0;
 }
 
