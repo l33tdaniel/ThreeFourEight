48af6bd3828a70ba8e70bd2fd07600cded1f25da
Brett Balquist <brett@balquist.com>
2023-10-17 13:07:16 -0500

    Added exponentiation issue #20

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <stdexcept>
+#include <cmath>
 int main(){
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
-
+//This takes the remainder of two numbers
 int modulo(int operand1, int operand2){
 	if (operand2 != 0){
 		return operand1 % operand2;
@@ -18,3 +19,9 @@ int modulo(int operand1, int operand2){
 int multiply(int a, int b) {
   return a * b;
 }
+
+//This takes a number and raises it to a power
+int exponentiation(int base, int power){
+	return pow(base,power);
+}
+
